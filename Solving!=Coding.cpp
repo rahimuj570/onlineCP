38fb43f1875bc322b5c531deaf057d0b9bb1b845
rahimuj570 <rahimuj570@gmail.com>
2023-01-25 15:13:36 +0600

    Solving != Coding icpc preliminary 2014

---
+++ Solving!=Coding.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+	// #define ll long long int
+	// #define fast ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+
+int main(){
+	// fast;
+	// #ifndef ONLINE_JUDGE
+	// freopen("inputf.in","r",stdin);
+	// freopen("outputf.out","w",stdout);
+	// #endif
+	//////////////////////////////////
+/*string s;
+int c=0;
+while(getline(cin,s))for(int i=0; i<s.length(); i++)if(s[i]=='A' || s[i]=='a')c++;
+from the above program, there are 97 A*/
+cout<<97<<'\n';
+return 0;}
