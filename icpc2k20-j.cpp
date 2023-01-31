5620543fe4c44bfd414702cfc3c6da6c4eb8fd98
rahimuj570 <rahimuj570@gmail.com>
2023-01-31 21:39:38 +0600

    https://algo.codemarshal.org/contests/icpc-dhaka-20-preli/problems/J

---
+++ icpc2k20-j.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+	using namespace std;
+	#define ll long long int
+	#define fast ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+
+int main(){
+fast;
+	// #ifndef ONLINE_JUDGE
+	// freopen("inputf.in","r",stdin);
+	// freopen("outputf.out","w",stdout);
+	// #endif
+	//////////////////////////////////
+	int n;
+	cin>>n;
+	n<6?cout<<1:cout<<(n/6)+1;
+
+return 0;}
