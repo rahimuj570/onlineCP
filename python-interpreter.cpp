5161baa990acf3e294e9c8b46a348c28e8436b8d
rahimuj570 <rahimuj570@gmail.com>
2023-02-06 13:02:22 +0600

    https://toph.co/p/python-interpreter

---
+++ python-interpreter.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+	using namespace std;
+	#define ll long long int
+	#define fast ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+
+
+int main(){
+fast;
+	#ifndef ONLINE_JUDGE
+	freopen("inputf.in","r",stdin);
+	freopen("outputf.out","w",stdout);
+	#endif
+	//////////////////////////////
+
+string s;
+cin>>s;
+for (int i = 7; i < s.size()-2; ++i)cout<<s[i];
+
+return 0;}
