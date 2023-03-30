7aa7c2398cfba87139c9a3f39debfa6fc9977ef4
rahimuj570 <rahimuj570@gmail.com>
2023-03-30 23:17:53 +0600

    Next Greater Element hackerrank using upperbound

---
+++ Next Greater Element_hr_upperbound.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+        #define ll long long int
+        #define fast ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+
+
+int main(){
+fast;
+        #ifndef ONLINE_JUDGE
+        freopen("inputf.in","r",stdin);
+        freopen("outputf.out","w",stdout);
+        #endif
+        //////////////////////////////////
+
+int n,input;
+cin>>n;
+std::vector<int> v;
+for(int i=0; i<n; i++){cin>>input;v.push_back(input);}
+std::vector<int> original_v(v.begin(), v.end());
+for(int i=0; i<n; i++){
+        auto it=upper_bound(v.begin(), v.end(),v[i]);
+        if(it!=v.end())v[i]=*it;
+        else v[i]=-1;
+      }
+for(int i=0; i<n; i++){cout<<original_v[i]<<' '<<v[i]<<endl;}
+
+return 0;}
